ddb49994e30d5c51604937b442aa67553a641313
tienv1per <tienv1perds@gmail.com>
2023-10-09 16:18:10 +0700

    add index to DB to faster query

--- core/Database.php
+++ core/Database.php
@@ -10,7 +10,7 @@ class Database {
     public static Database $db;
 
     public function __construct() {
-        $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=TrueP', 'root', '');
+        $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=AccountBase', 'root', '');
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         self::$db = $this;
     }
